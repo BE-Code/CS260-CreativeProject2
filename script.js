019a7cae768ead4b39d1aa039e630a631b8f2a01
Bradley English <bradley.english3@gmail.com>
2022-10-15 23:33:49 +0000

    new api

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const watchmodeKey = 'KymHqOJePgRiC2m3lXBM49ohK4n9KTA5yZ6a6CJe';
+const omdbKey = 'd3e7890';
 
 
 
@@ -10,28 +11,28 @@ document.getElementById("searchSubmit").addEventListener("click", function(event
         return;
     // console.log(value);
 
-
+    const url = "https://www.omdbapi.com/?i=tt3896198&apikey=" + omdbKey + "&s=" + value;
     // const url = "https://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=" + APIKEY;
-    // fetch(url)
-    //     .then(function(response) {
-    //         return response.json();
-    //     }).then(function(json) {
-    //         // console.log(json);
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            console.log(json);
 
 
-    //         let results = "";
-    //         results += '<h2>Weather in ' + json.name + "</h2>";
-    //         for (let i = 0; i < json.weather.length; i++) {
-    //             results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
-    //         }
-    //         results += '<h2>' + json.main.temp + " &deg;F</h2>"
-    //         results += "<p>"
-    //         for (let i = 0; i < json.weather.length; i++) {
-    //             results += json.weather[i].description
-    //             if (i !== json.weather.length - 1)
-    //                 results += ", "
-    //         }
-    //         results += "</p>";
-    //         document.getElementById("weatherResults").innerHTML = results;
-    //     });
+            // let results = "";
+            // results += '<h2>Weather in ' + json.name + "</h2>";
+            // for (let i = 0; i < json.weather.length; i++) {
+            //     results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
+            // }
+            // results += '<h2>' + json.main.temp + " &deg;F</h2>"
+            // results += "<p>"
+            // for (let i = 0; i < json.weather.length; i++) {
+            //     results += json.weather[i].description
+            //     if (i !== json.weather.length - 1)
+            //         results += ", "
+            // }
+            // results += "</p>";
+            // document.getElementById("weatherResults").innerHTML = results;
+        });
 });
