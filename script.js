ccc5c592555e63ffd258cb5f11e787d166cb0ee9
Taylor English <taylorjenglish@gmail.com>
2022-10-15 23:43:12 -0600

    merge code

--- script.js
+++ script.js
@@ -151,3 +151,30 @@ function autocomplete(inp) {
 }
 
 autocomplete(document.getElementById("searchInput"));
+
+
+/*************************** Search Results ***************************************/
+const moviePosterList = document.getElementsByClassName('poster');
+
+document.addEventListener('click', selectMovie);
+
+function selectMovie(event) {
+    const searchBar = document.getElementsByTagName('form')[0];
+    searchBar.style.display = 'none';
+    
+    let moviePoster = event.target;
+    if (!moviePoster.classList.contains('poster')) return;
+
+    let imdbID = moviePoster.getAttribute('data-imdb');
+    showMovieInfo(imdbID);
+}
+
+function showMovieInfo(imdbID) {
+    const resultsDiv = document.getElementById('results');
+    const movieInfoDiv = document.getElementById('movieInfo');
+
+    resultsDiv.style.display = 'none';
+    movieInfoDiv.style.display = 'flex';
+
+}
+/*************************** End Search Results ***************************************/
